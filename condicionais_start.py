acf302283f9238bffa36fba56fc44e88c40bd736
Deise Freire <51007898+DeiseFreire@users.noreply.github.com>
2021-05-26 15:00:55 -0300

    Estruturas condicionais

---
+++ condicionais_start.py
@@ -0,0 +1,14 @@
+#
+# Arquivo de exemplo das estruturas condicinais  
+#
+
+def Condicionais():
+    x, y = 1000, 1000
+
+    if (x < y):
+         print ("x é menor que y")
+    elif (x == y):
+         print("x é igual a y")
+    else: 
+         print ("x é maior do que y")
+Condicionais ()

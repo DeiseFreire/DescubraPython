b96dea5ec97f78262aca27daf6a692e9141d481f
Deise Freire <51007898+DeiseFreire@users.noreply.github.com>
2021-05-27 08:23:53 -0300

    Usando intervalos de datas

---
+++ delta_start.py
@@ -0,0 +1,14 @@
+from datetime import date
+from datetime import time
+from datetime import datetime
+from datetime import timedelta
+
+def deltaTempo():
+    delta = timedelta(day = 86, hours = 8532, minutes = 7645)
+    print(delta)
+
+    hoje = datetime.now()
+    print("Data no futuro: ", hoje + delta)
+    print("Data no passado: ", hoje + delta)
+
+deltaTempo()

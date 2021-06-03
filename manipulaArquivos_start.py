6e86030d80da2a19e515b641c5423458270bf8ca
Deise Freire <51007898+DeiseFreire@users.noreply.github.com>
2021-06-03 14:59:32 -0300

    Outras manipulações de arquivos

--- manipulaArquivos_start.py
+++ manipulaArquivos_start.py
@@ -1,23 +1,22 @@
 #
-# Exemplos de como trabalhar com arquivos
-# 
+# Exemplos de como trabalhar com arquivos 
+#
 
-import os 
+import os
 from os import path
 import shutil
 
-def copiarArquivo():
+def copiaArquivo():
     if path.exists("NovoArquivo.txt"):
         pathAtual = path.realpath("NovoArquivo.txt")
         novoPath = pathAtual + ".bkp"
         shutil.copy(pathAtual, novoPath)
-
         shutil.copystat(pathAtual, novoPath)
 
-#copiarArquivo()
+# copiaArquivo()
 
 def renomearArquivo():
     if path.exists("NovoArquivo.txt.bkp"):
-       os.rename("NovoArquivo.txt.bkp", "ArquivoAlterado.txt")
-renomearArquivo()     
-
+        os.rename("NovoArquivo.txt.bkp", "ArquivoAlterado.txt")
+        
+renomearArquivo()
